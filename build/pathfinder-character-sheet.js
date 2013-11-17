14711361cd8ad270defaec854cdeaf59a360c1aa
Travis Mitchell <contact@traviscmitchell.com>
2013-11-16 23:11:59 -0800

    Ability table pretty much done

--- build/pathfinder-character-sheet.js
+++ build/pathfinder-character-sheet.js
@@ -4232,7 +4232,18 @@ ko.exportSymbol('nativeTemplateEngine', ko.nativeTemplateEngine);
 var ko = require("knockout");
 
 var ability = function(name, code) {
-    var score = ko.observable(0);
+    var base = ko.observable(0);
+    var enhance = ko.observable(0);
+    var misc = ko.observable(0);
+    var temp = ko.observable(0);
+
+    // todo: extend for ints
+    var score = ko.computed(function() {
+        return parseInt(base(), 10) +
+            parseInt(enhance(), 10) +
+            parseInt(misc(), 10) +
+            parseInt(temp(), 10);
+    });
 
     var modifier = ko.computed(function() {
         return Math.floor((score() - 10) / 2);
@@ -4241,8 +4252,12 @@ var ability = function(name, code) {
     return {
         name: name,
         code: code,
-        score: score,
-        modifier: modifier
+        base: base,
+        enhance: enhance,
+        misc: misc,
+        temp: temp,
+        modifier: modifier,
+        score: score
     }
 }
 
--- src/ability.js
+++ src/ability.js
@@ -4,7 +4,18 @@
 var ko = require("knockout");
 
 var ability = function(name, code) {
-    var score = ko.observable(0);
+    var base = ko.observable(0);
+    var enhance = ko.observable(0);
+    var misc = ko.observable(0);
+    var temp = ko.observable(0);
+
+    // todo: extend for ints
+    var score = ko.computed(function() {
+        return parseInt(base(), 10) +
+            parseInt(enhance(), 10) +
+            parseInt(misc(), 10) +
+            parseInt(temp(), 10);
+    });
 
     var modifier = ko.computed(function() {
         return Math.floor((score() - 10) / 2);
@@ -13,8 +24,12 @@ var ability = function(name, code) {
     return {
         name: name,
         code: code,
-        score: score,
-        modifier: modifier
+        base: base,
+        enhance: enhance,
+        misc: misc,
+        temp: temp,
+        modifier: modifier,
+        score: score
     }
 }
 
