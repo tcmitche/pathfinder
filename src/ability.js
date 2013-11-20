af6ef219dda63733bc8cfea73a88272e0e0fd5ad
Travis Mitchell <contact@traviscmitchell.com>
2013-11-19 18:28:33 -0800

    Fixed racial modifiers to account for '+2 to any' races

--- src/ability.js
+++ src/ability.js
@@ -18,22 +18,34 @@ var friendlyName = function(code) {
 
 var ability = function(code, base, racialMod, enhance, misc, temp) {
     // I hate this, must fix later
-    base = _.isNumber(base) ? base : 0;
-    enhance = _.isNumber(enhance) ? enhance : 0;
-    misc = _.isNumber(misc) ? misc : 0;
-    temp = _.isNumber(temp) ? temp : 0;
+    base = _(base).isNumber() ? base : 0;
+    enhance = _(enhance).isNumber() ? enhance : 0;
+    misc = _(misc).isNumber() ? misc : 0;
+    temp = _(temp).isNumber() ? temp : 0;
     
     base = ko.observable(base).extend({ integer: null });
+    var racialValue = ko.observable(0).extend({ integer: null });
+    // Todo: it would be nice if there was an alert if a score was missing from the ability total
     racial = ko.computed(function() {
+        if (_(racialMod().ANY).isNumber()) {
+            racialValue(0);
+            return "(" + racialMod().ANY + ")";
+        } else if (_(racialMod()[code]).isNumber()) {
+            racialValue(racialMod()[code])
+            return racialMod()[code];
+        } else {
+            racialValue(0);
+            return 0;
+        }
         return racialMod()[code] || 0; // There's a special case for 'ANY' here (e.g. Humans)
-    }).extend({ integer: null });
+    });
     enhance = ko.observable(enhance).extend({ integer: null });
     misc = ko.observable(misc).extend({ integer: null });
     temp = ko.observable(temp).extend({ integer: null });
 
     // todo: extend for ints
     var score = ko.computed(function() {
-        return base() + racial() + enhance() + misc() + temp();
+        return base() + racialValue() + enhance() + misc() + temp();
     });
 
     var modifier = ko.computed(function() {
